19b59391e33dddd87c6670ff9e594db8e776edf6
Barrenhyatt0809 <1666090780@qq.com>
2020-09-16 17:19:33 +0800

    Signed-off-by: Barrenhyatt0809 <1666090780@qq.com>

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// 引用组件
+import Home from './views/Home';
+import About from './views/About';
 
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {};
+  }
+
+  render(){
+    return (
+      <HashRouter>    {/* HashRouter 使用URL中的hash(#)部分去创建路由，访问地址就会变成 这种：http://localhost:3000/#/about */}
+        <Switch>      {/* <Route>在Switch中路径相同的情况下，匹配第一个，避免重复匹配*/}
+          <Route exact component={Home}  path="/"/> {/* exact 精准匹配 */}
+          <Route component={About} path="/about"/>
+        </Switch>
+      </HashRouter>
+      )
+    }
+  }
 export default App;
